013fe3ef9274bda2b2164b807c7b0127f9de2804
yiyezhiqiu1024 <359297567@qq.com>
2021-04-10 10:12:10 +0800

    显示客户列表

---
+++ 01_crm/src/com/sl/bean/Customer.java
@@ -0,0 +1,37 @@
+package com.sl.bean;
+
+public class Customer {
+    private String name;
+    private Integer age;
+    private Double height;
+
+    public Customer(String name, Integer age, Double height) {
+        this.name = name;
+        this.age = age;
+        this.height = height;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public Double getHeight() {
+        return height;
+    }
+
+    public void setHeight(Double height) {
+        this.height = height;
+    }
+}
--- 01_crm/src/com/sl/servlet/LoginServlet.java
+++ 01_crm/src/com/sl/servlet/LoginServlet.java
@@ -1,5 +1,7 @@
 package com.sl.servlet;
 
+import com.sl.bean.Customer;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -7,6 +9,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
 
 @WebServlet("/login")
 public class LoginServlet extends HttpServlet {
@@ -28,10 +32,10 @@ public class LoginServlet extends HttpServlet {
      */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        _outHTML(request, response);
+        outHTML(request, response);
     }
 
-    private void _outHTML(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    private void outHTML(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         // 1.设置请求数据的编码
         request.setCharacterEncoding("UTF-8");
 
@@ -47,24 +51,66 @@ public class LoginServlet extends HttpServlet {
 
         // 5.判断用户名、密码是否正确（假设规定用户名密码都是123，才代表正确）
         if ("123".equals(username) && "123".equals(password)) {
-            out.write("<h1 style=\"color: blue; border: 1px solid black;\">登录成功</h1>");
-            out.write("<ul>");
-            out.write("<li>个人信息</li>");
-            out.write("<li>修改密码</li>");
-            out.write("<a href=\"http://localhost:8080/crm/page/login/index.html\">退出登录</a>");
-            out.write("</ul>");
-            out.write("<ul>");
-            out.write("<li><a href=\"../list/index.jsp\">客户列表</a></li>");
-            out.write("</ul>");
+            success(out);
         } else {
-            out.write("<h1 style=\"color: red; border: 1px solid black;\">登录失败</h1>");
-            out.write("<ul>");
-            out.write("<a href=\"http://localhost:8080/crm/page/login/index.html\">重新登录</a>");
-            out.write("</ul>");
+            faile(out);
         }
     }
 
-    private void _outPlain(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    private void success(PrintWriter out) {
+        out.write("<html>");
+        out.write("<head>");
+        out.write("<link rel=\"stylesheet\" href=\"http://localhost:8080/crm/page/login/index.css\">");
+        out.write("</head>");
+        out.write("<h1 style=\"color: blue; border: 1px solid black;\">登录成功</h1>");
+        out.write("<ul>");
+        out.write("<li>个人信息</li>");
+        out.write("<li>修改密码</li>");
+        out.write("<a href=\"http://localhost:8080/crm/page/login/index.html\">退出登录</a>");
+        out.write("</ul>");
+        out.write("<h2>客户列表</h2>");
+        out.write("<table>");
+        out.write("<thead>");
+        out.write("<tr>");
+        out.write("<th>姓名</th>");
+        out.write("<th>电话</th>");
+        out.write("<th>性别</th>");
+        out.write("</tr>");
+        out.write("</thead>");
+        out.write("<tbody>");
+
+        List<Customer> customers = getCustomers();
+        for (Customer customer : customers) {
+            out.write("<tr>");
+            out.write("<td>" + customer.getName() + "</td>");
+            out.write("<td>" + customer.getAge() + "</td>");
+            out.write("<td>" + customer.getHeight() + "</td>");
+            out.write("</tr>");
+        }
+
+        out.write("</tbody>");
+        out.write("</table>");
+        out.write("</body>");
+        out.write("</html>");
+    }
+
+    private List<Customer> getCustomers() {
+        List<Customer> customers = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            Customer customer = new Customer("张三" + i, 15 + i, 155.0 + i);
+            customers.add(customer);
+        }
+        return customers;
+    }
+
+    private void faile(PrintWriter out) {
+        out.write("<h1 style=\"color: red; border: 1px solid black;\">登录失败</h1>");
+        out.write("<ul>");
+        out.write("<a href=\"http://localhost:8080/crm/page/login/index.html\">重新登录</a>");
+        out.write("</ul>");
+    }
+
+    private void outPlain(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         // 1.设置请求数据的编码
         request.setCharacterEncoding("UTF-8");
 
