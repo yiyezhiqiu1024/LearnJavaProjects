2220fb74fda44fbc15c8f1a125891dbe45dfb8e9
yiyezhiqiu1024 <359297567@qq.com>
2021-04-15 15:17:48 +0800

    1.教育经验列表 2.教育经验添加

---
+++ 02_resume/src/main/java/com/sl/dao/EducationDao.java
@@ -0,0 +1,36 @@
+package com.sl.dao;
+
+import com.sl.bean.Education;
+import com.sl.bean.Website;
+import com.sl.util.Dbs;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class EducationDao {
+
+    public List<Education> list() {
+        String sql = "SELECT id, created_time, name, type, intro, begin_day, end_day FROM education";
+        return Dbs.getTpl().query(sql, new BeanPropertyRowMapper<>(Education.class));
+    }
+
+    public boolean save(Education education) {
+        List<Object> args = new ArrayList<>();
+        args.add(education.getName());
+        args.add(education.getType());
+        args.add(education.getIntro());
+        args.add(education.getBeginDay());
+        args.add(education.getEndDay());
+        Integer id = education.getId();
+        String sql;
+        if (id == null || id < 1) { // 添加
+            sql = "INSERT INTO education(name, type, intro, begin_day, end_day) VALUES (?, ?, ?, ?, ?)";
+        } else { // 更新
+            sql = "UPDATE education SET name = ?, type = ?, intro = ?, begin_day = ?, end_day = ? WHERE id = ?";
+            args.add(id);
+        }
+
+        return Dbs.getTpl().update(sql, args.toArray()) > 0;
+    }
+}
---
+++ 02_resume/src/main/java/com/sl/servlet/EducationServlet.java
@@ -0,0 +1,38 @@
+package com.sl.servlet;
+
+import com.sl.bean.Education;
+import com.sl.dao.EducationDao;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.text.SimpleDateFormat;
+import java.util.List;
+
+@WebServlet("/education/*")
+public class EducationServlet extends BaseServlet {
+    private final EducationDao dao = new EducationDao();
+
+    public void admin(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        List<Education> educations = dao.list();
+        request.setAttribute("educations", educations);
+        request.getRequestDispatcher("/page/admin/education.jsp").forward(request, response);
+    }
+
+    public void save(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        Education education = new Education();
+        education.setName(request.getParameter("name"));
+        education.setIntro(request.getParameter("intro"));
+        education.setType(Integer.valueOf(request.getParameter("type")));
+        SimpleDateFormat fmt = new SimpleDateFormat("yyyy-MM-dd");
+        education.setBeginDay(fmt.parse(request.getParameter("beginDay")));
+        education.setEndDay(fmt.parse(request.getParameter("endDay")));
+        if (dao.save(education)) {
+            response.sendRedirect( request.getContextPath()+ "/education/admin");
+        } else {
+            forwardError(request, response, "保存网站信息失败");
+        }
+    }
+
+
+}
