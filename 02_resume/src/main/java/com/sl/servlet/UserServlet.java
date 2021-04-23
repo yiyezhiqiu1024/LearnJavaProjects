75c562040f7b6956171d1dcc9f666a1483c6a23b
yiyezhiqiu1024 <359297567@qq.com>
2021-04-23 14:34:38 +0800

    验证码

--- 02_resume/src/main/java/com/sl/servlet/UserServlet.java
+++ 02_resume/src/main/java/com/sl/servlet/UserServlet.java
@@ -1,12 +1,18 @@
 package com.sl.servlet;
 
+import com.google.code.kaptcha.impl.DefaultKaptcha;
+import com.google.code.kaptcha.util.Config;
 import com.sl.bean.User;
 import com.sl.service.UserService;
 import org.apache.commons.beanutils.BeanUtils;
 
+import javax.imageio.ImageIO;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.awt.image.BufferedImage;
+import java.io.InputStream;
+import java.util.Properties;
 
 @WebServlet("/user/*")
 public class UserServlet extends BaseServlet<User> {
@@ -32,5 +38,31 @@ public class UserServlet extends BaseServlet<User> {
         } else { // 用户名、密码有问题
             forwardError(request, response, "邮箱或密码不正确");
         }
+    }
+
+    public void captcha(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        // 创建Katpcha对象
+        DefaultKaptcha dk = new DefaultKaptcha();
+
+        // 验证码的配置
+        try (InputStream is = getClass().getClassLoader().getResourceAsStream("kaptcha.properties")) {
+            Properties properties = new Properties();
+            properties.load(is);
+
+            Config config = new Config(properties);
+            dk.setConfig(config);
+        }
+
+        // 生成验证码字符串
+        String code = dk.createText();
+
+        // 生成验证码图片
+        BufferedImage image = dk.createImage(code);
+
+        // 设置返回数据的格式
+        response.setContentType("image/jpeg");
+
+        // 将图片数据写会到客户端
+        ImageIO.write(image, "jpg", response.getOutputStream());
     }
 }
