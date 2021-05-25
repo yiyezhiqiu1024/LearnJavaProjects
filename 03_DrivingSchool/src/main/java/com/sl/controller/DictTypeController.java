fd4b716905c44f833256928af3f45ca3143d9a08
yiyezhiqiu1024 <359297567@qq.com>
2021-05-25 17:23:41 +0800

    1.配置项目相关依赖   - spring-boot-starter-freemarker（模板引擎） 2.数据字典类型模块   - 添加静态资源（classpath:/static/目录下）   - 添加模板文件（classpath:/templates/目录下）   - 通过 DictType.ftlh 模板文件生成展示列表页面返回给客户端

--- 03_DrivingSchool/src/main/java/com/sl/controller/DictTypeController.java
+++ 03_DrivingSchool/src/main/java/com/sl/controller/DictTypeController.java
@@ -1,14 +1,11 @@
 package com.sl.controller;
 
-import com.sl.pojo.po.DictType;
 import com.sl.service.DictTypeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-import java.util.List;
 
 @Controller
 @RequestMapping("/dictTypes")
@@ -16,9 +13,9 @@ public class DictTypeController {
     @Autowired
     private DictTypeService service;
 
-    @GetMapping("list")
-    @ResponseBody
-    public List<DictType> list() {
-        return service.list();
+    @GetMapping("/list")
+    public String list(Model model) {
+        model.addAttribute("items", service.list());
+        return "pages/dictType";
     }
 }
