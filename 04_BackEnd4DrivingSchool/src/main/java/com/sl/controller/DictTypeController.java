5d93ecc2f24fe87759153191d0560972e27ddf95
yiyezhiqiu1024 <359297567@qq.com>
2021-05-28 14:26:14 +0800

    数据字典类型：删除一条或多条记录

--- 04_BackEnd4DrivingSchool/src/main/java/com/sl/controller/DictTypeController.java
+++ 04_BackEnd4DrivingSchool/src/main/java/com/sl/controller/DictTypeController.java
@@ -6,9 +6,11 @@ import com.sl.service.DictTypeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -23,12 +25,23 @@ public class DictTypeController {
     @GetMapping
     public Map<String, Object> list(DictTypeQuery query) {
         service.list(query);
-        List<DictType> records = query.getRecords();
         Map<String, Object> map = new HashMap<>();
         map.put("code", 0);
         map.put("msg", "请求成功");
-        map.put("count", records.size());
-        map.put("data", records);
+        map.put("count", query.getTotal());
+        map.put("data", query.getRecords());
         return map;
+    }
+
+    @PostMapping("/remove")
+    public Map<String, Object> remove(String id) {
+        if (service.removeByIds(Arrays.asList(id.split(",")))) {
+            Map<String, Object> map = new HashMap<>();
+            map.put("code", 0);
+            map.put("msg", "删除成功");
+            return map;
+        } else {
+            throw new RuntimeException("删除失败");
+        }
     }
 }
