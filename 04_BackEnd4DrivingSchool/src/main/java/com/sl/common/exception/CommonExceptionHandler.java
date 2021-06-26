10b657f847b68ce904aab943fb30ae2144937c45
yiyezhiqiu1024 <359297567@qq.com>
2021-06-26 09:14:04 +0800

    统一异常处理   - 设置返回数据格式的另一种方式   - @Slf4j 日志打印

--- 04_BackEnd4DrivingSchool/src/main/java/com/sl/common/exception/CommonExceptionHandler.java
+++ 04_BackEnd4DrivingSchool/src/main/java/com/sl/common/exception/CommonExceptionHandler.java
@@ -1,23 +1,29 @@
 package com.sl.common.exception;
 
-import com.sl.common.util.Debugs;
 import com.sl.common.util.Rs;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.nio.charset.StandardCharsets;
 
 @ControllerAdvice
+@Slf4j
 public class CommonExceptionHandler {
     @ExceptionHandler(Throwable.class)
     public void handle(Throwable t,
                        HttpServletRequest request,
                        HttpServletResponse response) throws Exception {
         // 设置返回数据格式
-        response.setContentType("application/json; charset=UTF-8");
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        request.setCharacterEncoding(StandardCharsets.UTF_8.displayName());
+//        response.setContentType("application/json; charset=UTF-8");
         response.setStatus(400);
         response.getWriter().write(Rs.error(t).jsonString());
-        Debugs.run(t::printStackTrace);
+//        Debugs.run(t::printStackTrace);
+        log.error(null, t);
     }
 }
