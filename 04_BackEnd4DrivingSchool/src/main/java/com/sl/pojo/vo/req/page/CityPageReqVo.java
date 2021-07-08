4bb8dcf40addb4f6154059e737a908e71ca945eb
yiyezhiqiu1024 <359297567@qq.com>
2021-07-08 16:50:06 +0800

    FIX：奇怪的bug（Swagger)

--- 04_BackEnd4DrivingSchool/src/main/java/com/sl/pojo/vo/req/page/CityPageReqVo.java
+++ 04_BackEnd4DrivingSchool/src/main/java/com/sl/pojo/vo/req/page/CityPageReqVo.java
@@ -1,12 +1,11 @@
 package com.sl.pojo.vo.req.page;
 
-import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
 @EqualsAndHashCode(callSuper = true)
 @Data
 public class CityPageReqVo extends KeywordPageReqVo {
-    @ApiModelProperty("省份id")
+//    @ApiModelProperty("省份id")
     public Integer parentId;
 }
